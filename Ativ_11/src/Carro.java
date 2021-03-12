d678fe8e5539ed0d7e09eb64224cea9dba1130ce
Thiago47-gif <th.ignacio47@gmail.com>
2021-03-12 17:09:32 -0300

    Atividade Fixacao Get Set

---
+++ Ativ_11/src/Carro.java
@@ -0,0 +1,21 @@
+
+public class Carro {
+	
+	private String Cor;
+	private String Ano;
+	
+	public void setCor(String cor) {
+		this.Cor = cor; 
+	}
+	public String getCor() {
+		return Cor;
+	}
+	
+	public void setAno(String ano) {
+		this.Ano = ano; 
+	}
+	public String getAno() {
+		return Ano;
+	}
+
+}
---
+++ Ativ_11/src/Fabrica.java
@@ -0,0 +1,20 @@
+
+public class Fabrica {
+	public static void main(String[] args) {
+		
+		Carro carro01;
+		carro01 = new Carro();
+		
+		//Salvando informações
+		carro01.setCor("Azul");
+		carro01.setAno("2011");
+		//Salvar as demais informações
+		
+		//Pegando as informações para imprimir
+		System.out.println("Dados do Carro:");
+		System.out.println("Cor: " + carro01.getCor());
+		System.out.println("Ano: " + carro01.getAno());
+		//Imprimir as demais informações
+	}
+
+}
