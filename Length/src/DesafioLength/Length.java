d2e5f9393a8858ab16223d00fe5b01faf5781675
Thiago47-gif <th.ignacio47@gmail.com>
2021-04-01 15:31:26 -0300

    Desafio Length

---
+++ Length/src/DesafioLength/Length.java
@@ -0,0 +1,15 @@
+package DesafioLength;
+
+public class Length {
+
+	public static void main(String[] args) {
+		
+		String nome = "Robert Lewandowiski";
+		
+		int tamanho = nome.length();//Retorna o tamanho da String
+		
+		System.out.println(tamanho);
+
+	}
+
+}
