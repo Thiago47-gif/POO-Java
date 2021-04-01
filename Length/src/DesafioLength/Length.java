7d4335241f656c6bdd82954ab437dda8d8e5fdf8
Thiago47-gif <th.ignacio47@gmail.com>
2021-04-01 15:44:34 -0300

    Desafio Length

--- Length/src/DesafioLength/Length.java
+++ Length/src/DesafioLength/Length.java
@@ -4,11 +4,11 @@ public class Length {
 
 	public static void main(String[] args) {
 		
-		String nome = "*************************\n******CADÊ A VACINA******\n*************************";
+		String nome = "   *************************\n   ******CADÊ A VACINA******\n   *************************";
 		
 		int tamanho = nome.length();//Retorna o tamanho da String
 		System.out.println(nome + "\n\n");
-		System.out.println(tamanho +" Caracteres existentes");
+		System.out.println("   "+ tamanho +" Caracteres existentes");
 
 	}
 
